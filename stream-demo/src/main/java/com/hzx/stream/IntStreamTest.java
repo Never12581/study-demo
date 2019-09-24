c9ee99a3a701828228208ed50789eef7d2709163
huangbocai <huangbocai@cai-inc.com>
2019-09-24 17:12:45 +0800

    update java basics + 14

--- stream-demo/src/main/java/com/hzx/stream/IntStreamTest.java
+++ stream-demo/src/main/java/com/hzx/stream/IntStreamTest.java
@@ -1,6 +1,10 @@
 package com.hzx.stream;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.OptionalInt;
+import java.util.function.IntPredicate;
+import java.util.function.ToLongFunction;
 import java.util.stream.IntStream;
 
 /**
@@ -15,16 +19,29 @@ public class IntStreamTest {
         IntStream intStream = IntStream.of(new int[]{1, 2, 3});
         intStream.forEach(System.out::println);
         splitLine();
-        IntStream.of(new int[]{1, 2, 3}).forEach(System.out::println);
+        IntStream.of(new int[]{1, 2, 3}).filter(e -> e==1).forEach(System.out::println);
         splitLine();
         IntStream.range(1, 3).forEach(System.out::println);
         splitLine();
         OptionalInt optionalInt = IntStream.of(new int[]{1, 2, 3}).findFirst();
         System.out.println(optionalInt.getAsInt());
+
+        splitLine();
+
+        String s = "123";
+        long l = toLong(s,k ->  Long.parseLong(k));
+        System.out.println("s:"+s +" l :"+l);
+
+        long c = toLong(1,k-> (long)k);
+        System.out.println("传入: 1   c:"+c);
     }
 
     private static void splitLine(){
         System.out.println("==========================");
+    }
+
+    private static <T> long toLong (T str , ToLongFunction<T> toLongFunction) {
+        return toLongFunction.applyAsLong(str);
     }
 
 }
