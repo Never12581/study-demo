741413a17ce038fe5111ecef415c7431d2c29a6e
Never12581 <azaizai@icloud.com>
2020-05-20 11:40:48 +0800

    jaeger + java demo +1

--- data/src/main/java/com/hzx/data/controller/TutorialController.java
+++ data/src/main/java/com/hzx/data/controller/TutorialController.java
@@ -12,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
 import java.util.Collection;
 import java.util.NoSuchElementException;
 
@@ -29,6 +30,7 @@ public class TutorialController {
     }
 
 
+    @PostConstruct
     public void init() {
 
         // Add some employees
