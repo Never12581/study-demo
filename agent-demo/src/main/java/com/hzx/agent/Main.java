84ea8443f996133ec482af15be0ed7d3e3e1bf6a
Never12581 <azaizai@icloud.com>
2022-06-15 21:27:13 +0800

    grpc commit

--- agent-demo/src/main/java/com/hzx/agent/Main.java
+++ agent-demo/src/main/java/com/hzx/agent/Main.java
@@ -3,6 +3,7 @@ package com.hzx.agent;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("我这里是主逻辑!");
+        User abc = new User("abc", 12);
+        System.out.println(abc);
     }
 }
---
+++ agent-demo/src/main/java/com/hzx/agent/Model.kt
@@ -0,0 +1,3 @@
+package com.hzx.agent
+
+data class User(val name: String, val age: Int)
